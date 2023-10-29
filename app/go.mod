3437e28d7fa14ed27c2ef98571dcb26a75333f1d
seiya_tagami <bepozurapu274@gmail.com>
2023-10-29 11:24:52 +0900

    fixing router

--- app/go.mod
+++ app/go.mod
@@ -3,8 +3,8 @@ module github.com/Seiya-Tagami/favorite-character-api
 go 1.20
 
 require (
+	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.9.1
-	github.com/go-playground/validator/v10 v10.14.0
 	gorm.io/driver/postgres v1.5.2
 	gorm.io/gorm v1.25.3
 )
@@ -16,6 +16,7 @@ require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
@@ -24,7 +25,6 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
-	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
--- app/router/router.go
+++ app/router/router.go
@@ -1,20 +1,39 @@
 package router
 
 import (
+	"time"
+
 	"github.com/Seiya-Tagami/favorite-character-api/handler/character"
 	"github.com/Seiya-Tagami/favorite-character-api/handler/health"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func New(healthHandler health.HealthHandler, characterHandler character.Handler) *gin.Engine {
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "PATCH", "PUT", "DELETE"},
+		AllowHeaders: []string{
+			"Access-Control-Allow-Origin",
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"Authorization",
+		},
+		AllowCredentials: true,
+		MaxAge:           24 * time.Hour,
+	}))
+
 	router.GET("/health", healthHandler.HealthCheck)
 
 	charactersRouter := router.Group("/characters")
 	{
-		charactersRouter.GET("/", characterHandler.ListCharacters)
+		charactersRouter.GET("", characterHandler.ListCharacters)
 		charactersRouter.GET("/:id", characterHandler.FindCharacter)
-		charactersRouter.POST("/", characterHandler.CreateCharacter)
+		charactersRouter.POST("", characterHandler.CreateCharacter)
 		charactersRouter.DELETE("/:id", characterHandler.DeleteCharacter)
 		charactersRouter.PATCH("/:id", characterHandler.UpdateCharacter)
 	}
